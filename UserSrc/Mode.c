142cd6c070c0d34cb74d82574e4e745cd5ff8cd3
penhirai <hirai.verdi@gmail.com>
2022-03-11 16:20:03 +0900

    adjust velocity

--- UserSrc/Mode.c
+++ UserSrc/Mode.c
@@ -58,23 +58,23 @@ void MDE_SelectMode(void)
 			case CASE_SEARCH_MODE:
 				if(st_Decision == SWT_DECISION_TRUE)
 				{
-					TRC_StartSearchMode(1.2, 1.2);
+					TRC_StartSearchMode(1.3, 1.2);
 				}
 				break;
 			case CASE_TRACE_MODE:
 				if(st_Decision == SWT_DECISION_TRUE)
 				{
-//					CVL_SetTargetUpAccel(7.0);
-//					CVL_SetTargetDownAccel(7.0);
-					TRC_StartSearchMode(1.3, 1.2);
+					CVL_SetTargetUpAccel(7.0);
+					CVL_SetTargetDownAccel(7.0);
+					TRC_StartSearchMode(1.4, 1.2);
 					//TRC_StartDriveMode();
 				}
 				break;
 			case CASE_TRACE2_MODE:
 				if(st_Decision == SWT_DECISION_TRUE)
 				{
-//					CVL_SetTargetUpAccel(7.0);
-//					CVL_SetTargetDownAccel(7.0);
+					CVL_SetTargetUpAccel(9.0);
+					CVL_SetTargetDownAccel(9.0);
 					TRC_StartSearchMode(1.4, 1.3);
 					//TRC_StartDriveMode();
 				}
